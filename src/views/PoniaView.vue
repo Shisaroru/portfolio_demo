035f35d26c03718fc99a667aa63c2c43fb4d465f
Thanh <thanhminhtrinh78@gmail.com>
2026-06-07 21:22:50 +0700

    feat: fix some more layout

--- src/views/PoniaView.vue
+++ src/views/PoniaView.vue
@@ -35,7 +35,7 @@ const textHolder =
   'Tại Tôn Pomina, tôi tham gia sản xuất nhiều thể loại video Marketing khác nhau, từ video sản phẩm đến phóng sự khách hàng và video ADS. Tôi truyền tải những thông điệp truyền thống theo cách riêng hiện đại, nhưng vẫn giữ được giá trị cốt lõi của thương hiệu.Thử thách lớn nhất là làm sao để một sản phẩm kỹ thuật trở nên gần gũi với người xem. Thông qua cách kể chuyện bằng hình ảnh sáng tạo, cấu trúc nội dung rõ ràng và nhịp dựng phù hợp, tôi dần học được cách cân bằng giữa tính sáng tạo của nội dung và mục tiêu truyền thông của doanh nghiệp.'
 
 const mainTextContent =
-  'Trong quá trình công tác tại Tôn Pomina, tôi có cơ hội đi thực tế tại nhiều vùng miền khác nhau. Những trải nghiệm này giúp tôi hiểu rõ hơn nhu cầu, hành vi và cách tiếp nhận thông tin của khách hàng, đồng thời tôi nhận ra rằng việc thấu hiểu văn hóa đại chúng, văn hóa địa phương là yếu tố quan trọng để xây dựng nội dung truyền thông phù hợp và  hiệu quả.'
+  'Trong quá trình công tác tại Tôn Pomina, tôi có cơ hội đi thực tế tại nhiều vùng miền khác nhau. Những trải nghiệm này giúp tôi hiểu rõ hơn nhu cầu, hành vi và cách tiếp nhận thông tin của khách hàng, đồng thời tôi nhận ra rằng việc thấu hiểu văn hóa đại chúng, văn hóa địa phương là yếu tố quan trọng để xây dựng nội dung truyền thông phù hợp và hiệu quả.'
 </script>
 
 <style scoped>
